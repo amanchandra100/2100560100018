821ec481ec3861188246db37248af1c7904716ef
Aman Chandra <aman123chandra@gmail.com>
2024-08-20 22:46:09 +0530

    Final commit

--- answer1/Solutions/controller/categoryController.js
+++ answer1/Solutions/controller/categoryController.js
@@ -2,20 +2,19 @@ import axios from "axios";
 
 
 
-const token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzI0MTY2MjkyLCJpYXQiOjE3MjQxNjU5OTIsImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6IjA0MDY5ZjVjLTRhNTItNDEzMi1hNDc5LWU4MGIxZDYxOWNhYyIsInN1YiI6ImFtYW5jaGFuZHJhMjMxMDAxQGdtYWlsLmNvbSJ9LCJjb21wYW55TmFtZSI6IkJCRE5JSVQiLCJjbGllbnRJRCI6IjA0MDY5ZjVjLTRhNTItNDEzMi1hNDc5LWU4MGIxZDYxOWNhYyIsImNsaWVudFNlY3JldCI6IkFncFJXWmVmUlhqdmtUU3oiLCJvd25lck5hbWUiOiJBbWFuQ2hhbmRyYSIsIm93bmVyRW1haWwiOiJhbWFuY2hhbmRyYTIzMTAwMUBnbWFpbC5jb20iLCJyb2xsTm8iOiIyMTAwNTYwMTAwMDE4In0.CU7yjXxq10J72bu90h7revrcLt_5H_pfy3iEMi8D9d4";
-
-
-axios.defaults.headers.common["Authorization"] = token;
+const TOKEN="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzI0MTY2MjkyLCJpYXQiOjE3MjQxNjU5OTIsImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6IjA0MDY5ZjVjLTRhNTItNDEzMi1hNDc5LWU4MGIxZDYxOWNhYyIsInN1YiI6ImFtYW5jaGFuZHJhMjMxMDAxQGdtYWlsLmNvbSJ9LCJjb21wYW55TmFtZSI6IkJCRE5JSVQiLCJjbGllbnRJRCI6IjA0MDY5ZjVjLTRhNTItNDEzMi1hNDc5LWU4MGIxZDYxOWNhYyIsImNsaWVudFNlY3JldCI6IkFncFJXWmVmUlhqdmtUU3oiLCJvd25lck5hbWUiOiJBbWFuQ2hhbmRyYSIsIm93bmVyRW1haWwiOiJhbWFuY2hhbmRyYTIzMTAwMUBnbWFpbC5jb20iLCJyb2xsTm8iOiIyMTAwNTYwMTAwMDE4In0.CU7yjXxq10J72bu90h7revrcLt_5H_pfy3iEMi8D9d4";
 
 export const categoryController = async (red, res) => {
+    let {companyname , categoryname} = req.params; 
+    let {top, minPrice, maxPrice} = req.query; 
   try {
-    const data = await axios.get(`http://20.244.56.144/test/companies/AMZ/categories/Laptop/products?top=10&minPrice=1&maxPrice=10000`,
+    // const token = await axios.get(`http://20.244.56.144/test/auth`,)
+    let result = await axios.get(`http://20.244.56.144/test/companies/${companyname}/categories/${categoryname}/products?top=${top}&minPrice=${minPrice}&maxPrice=${maxPrice}`, 
       {
-        headers:{
-          'Authorization': `${token}`
-        }
-      }
-    );
+          headers: {
+            Authorization: `Bearer ${TOKEN}`
+          }
+      });
     // res.json(data)
     res.status(200).send({
         data,
--- answer1/Solutions/routes/categoryRoutes.js
+++ answer1/Solutions/routes/categoryRoutes.js
@@ -4,6 +4,6 @@ import { categoryController } from "../controller/categoryController.js";
 const router = express.Router();
 
 //getALl category
-router.get("/categories", categoryController);
+router.get("/:companyname/category/:categoryname/products", categoryController);
 
 export default router;
